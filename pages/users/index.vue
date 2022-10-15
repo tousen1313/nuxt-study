375aac85d2f3dee5b1f44558cfaae5198d13b6ab
tousen1313 <a09025340241@gmail.com>
2022-10-15 16:06:41 +0900

    init

---
+++ pages/users/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+div
+  p {{ name }} ({{ age }})
+  p Triple score: {{ triple }}
+  button(@click="onClickButton") Click Me!
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class SampleComponent extends Vue {
+  name = "simochee";
+  age = 21;
+  score = 55;
+
+  mounted() {
+    // コンポーネントがマウントされたときの処理
+    console.log("life cycle hook：mounted");
+  }
+
+  beforeDestroy() {
+    // コンポーネントが破棄される直前の処理
+    console.log("life cycle hook：beforeDestroy");
+  }
+
+  get triple() {
+    return this.score * 3;
+  }
+
+  onClickButton() {
+    alert("button pushed!");
+  }
+}
+</script>
